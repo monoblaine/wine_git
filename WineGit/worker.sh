5a1a28365f0133273c058c6098c5d0a95079a8a0
Serhan Apaydın <zerhan@gmail.com>
2025-08-16 15:30:48 +0300

    refactor

--- WineGit/worker.sh
+++ WineGit/worker.sh
@@ -4,16 +4,16 @@ path_to_wine_git_folder="/home/serhan/Shared/wine_git"
 path_to_tmp="$path_to_wine_git_folder/tmp"
 
 exec_id=$1
-path_to_out_file="$path_to_tmp/out_$exec_id"
 shift
 is_input_redirected=$1
 shift
+path_to_out_file=$path_to_tmp/out_$exec_id
 
 if [[ "$is_input_redirected" == "0" ]]; then
-    git "$@" >$path_to_out_file
+    git "$@" >"$path_to_out_file"
 else
     path_to_in_file=$path_to_tmp/in_$exec_id
-    git "$@" >$path_to_out_file <"$path_to_in_file"
+    git "$@" >"$path_to_out_file" <"$path_to_in_file"
     rm "$path_to_in_file"
 fi
 
