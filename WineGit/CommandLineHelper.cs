1f45fa2582962bc3aedee43262e318e2d5a5e047
Serhan Apaydın <zerhan@gmail.com>
2025-08-16 13:30:40 +0300

    Add note for CommandLineHelper.GetCommandLineW

--- WineGit/CommandLineHelper.cs
+++ WineGit/CommandLineHelper.cs
@@ -3,6 +3,12 @@ using System.Runtime.InteropServices;
 namespace WineGit;
 
 internal static partial class CommandLineHelper {
+    /// <summary>
+    /// "The lifetime of the returned value is managed by the system, applications should not free or modify this value."
+    ///
+    /// Source: https://learn.microsoft.com/en-us/windows/win32/api/processenv/nf-processenv-getcommandlinew
+    /// </summary>
+    /// <returns></returns>
     [LibraryImport("kernel32", StringMarshalling = StringMarshalling.Utf16)]
     private static partial IntPtr GetCommandLineW ();
 
