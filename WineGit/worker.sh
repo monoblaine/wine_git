dc5a0be2a52751f536e4678bdf272dde9aab964c
Serhan Apaydın <zerhan@gmail.com>
2025-08-16 15:28:50 +0300

    lint

--- WineGit/worker.sh
+++ WineGit/worker.sh
@@ -3,8 +3,8 @@
 path_to_wine_git_folder="/home/serhan/Shared/wine_git"
 path_to_tmp="$path_to_wine_git_folder/tmp"
 
-execId=$1
-path_to_out_file="$path_to_tmp/out_$execId"
+exec_id=$1
+path_to_out_file="$path_to_tmp/out_$exec_id"
 shift
 is_input_redirected=$1
 shift
@@ -12,9 +12,9 @@ shift
 if [[ "$is_input_redirected" == "0" ]]; then
     git "$@" >$path_to_out_file
 else
-    path_to_in_file=$path_to_tmp/in_$execId
+    path_to_in_file=$path_to_tmp/in_$exec_id
     git "$@" >$path_to_out_file <"$path_to_in_file"
     rm "$path_to_in_file"
 fi
 
-touch "$path_to_tmp/lock_$execId"
+touch "$path_to_tmp/lock_$exec_id"
